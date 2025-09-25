e15a77aabec45ad1749f83dc03af87868b2a1278
иван лыжин <zagrebnoy.nik@mail.ru>
2025-09-25 16:02:31 +0500

    Task4

---
+++ Tyuiu.LyzhinIN.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.LyzhinIN.Sprint0.Task4.V0.Lib;
+Console.WriteLine(DataService.Addition(1, 5));
+Console.WriteLine(DataService.Substraction(15, 5));
+Console.WriteLine(DataService.Multiplication(10, 10));
+Console.WriteLine(DataService.Division(5, 5));
+Console.ReadKey();
