25b7ce17870e8e005e4e5d7daa9e62b855b3861f
иван лыжин <zagrebnoy.nik@mail.ru>
2025-09-25 13:42:03 +0500

    Task1

---
+++ Tyuiu.LyzhinIN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Иван";
+        int age = 23;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " года ");
+
+        Console.ReadKey();
+    }
+}
