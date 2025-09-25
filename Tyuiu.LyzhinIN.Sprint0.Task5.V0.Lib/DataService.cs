080f26ed5489e7d25c6bbd2650d6443d07a2872c
иван лыжин <zagrebnoy.nik@mail.ru>
2025-09-25 16:18:12 +0500

    Task5

---
+++ Tyuiu.LyzhinIN.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿namespace Tyuiu.LyzhinIN.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            if(b ==0)
+            {
+                Console.WriteLine("Переменная b = (0), на ноль делить нельзя", b);
+                return 0;
+            }
+            else
+            {
+                return a / b;
+            }
+            
+        }
+
+    }
+}
---
+++ Tyuiu.LyzhinIN.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.LyzhinIN.Sprint0.Task4.V0.Lib;
+Console.WriteLine("A + B = "+DataService.Addition(1, 5));
+Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+//Console.WriteLine("A / B = " + DataService.Division(5, 0));
+Console.WriteLine("A / B = " + DataService.Division(9, 3));
+Console.ReadKey();
