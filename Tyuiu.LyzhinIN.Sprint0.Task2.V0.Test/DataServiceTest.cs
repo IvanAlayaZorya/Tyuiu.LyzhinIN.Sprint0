f1b05a0f1c59a4dab03a4db3fde0ecb89707febf
иван лыжин <zagrebnoy.nik@mail.ru>
2025-09-25 15:20:34 +0500

    Task2

---
+++ Tyuiu.LyzhinIN.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.LyzhinIN.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.LyzhinIN.Sprint0.Task2.V0.Test
+
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckMessageValid()
+        {
+            var name = "Иван";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Иван", res);
+        }
+    }
+}
